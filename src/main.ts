00031a9ed311d4739379a11c6ab14674afa9fe31
Marcus Castilho <marcus.castilho@govone.digital>
2024-06-10 10:12:16 -0300

    feat: setup do projeto + swagger + prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'basic-auth-connect';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(
+    '/dev',
+    basicAuth(
+      process.env.SWAGGER_USERNAME_LOGIN,
+      process.env.SWAGGER_USERNAME_PASSWORD,
+    ),
+  );
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle(process.env.APPNAME)
+    .setDescription(
+      `Esta é a api responsável por gerenciar a aplicação ${process.env.APPNAME}.`,
+    )
+    .setVersion(process.env.APPVERSION)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('dev', app, document);
+  await app.listen(
+    process.env.AMBIENT === 'development' ? 3000 : process.env.PORT,
+  );
 }
 bootstrap();
